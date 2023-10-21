5ca4607065a9ea749147b14211d5b8f2e09754e0
GianmarioC <cerciellogianmario59@gmail.com>
2023-10-21 11:54:32 +0200

    i

--- src/main/java/com/gruppo1/noleggio/controller/HomeController.java
+++ src/main/java/com/gruppo1/noleggio/controller/HomeController.java
@@ -1,12 +1,13 @@
 package com.gruppo1.noleggio.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
     @GetMapping("/ciao")
     public String home() {
-        return "index"; 
+        return "ciaoo"; 
     }
 }
